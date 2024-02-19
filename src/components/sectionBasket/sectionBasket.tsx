f8b46d28ee7ef03bbc088ee36a13372f74b3308c
Kiryl1998 <kirill.solovev199888@gmail.com>
2024-02-19 18:39:17 +0100

    add swiper in favorite

--- src/components/sectionBasket/sectionBasket.tsx
+++ src/components/sectionBasket/sectionBasket.tsx
@@ -1,13 +1,12 @@
 import { useNavigate } from 'react-router-dom';
-import CardBasketLike from '../cardBasketLike/cardBasketLike';
 import Icon from '../icon/icon';
 import styleSectionBasket from './sectionBasket.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowLeftLong } from '@fortawesome/free-solid-svg-icons';
-import SimilarBook from '../similarBook/similarBook';
 
 interface PropsSectionBasket {
   card: JSX.Element;
+  swiper?: JSX.Element;
 }
 
 const SectionBasket = (props: PropsSectionBasket) => {
@@ -29,6 +28,7 @@ const SectionBasket = (props: PropsSectionBasket) => {
           />
           <h2 className={styleSectionBasket.title}>Your cart</h2>
           {props.card}
+          {props.swiper}
         </div>
       </div>
     </section>
--- src/pages/favorite/favorite.tsx
+++ src/pages/favorite/favorite.tsx
@@ -2,17 +2,21 @@ import CardFavorite from '../../components/cardFavorite/cardFavorite';
 import Footer from '../../components/footer/footer';
 import Header from '../../components/header/header';
 import SectionBasket from '../../components/sectionBasket/sectionBasket';
+import SimilarBook from '../../components/similarBook/similarBook';
 
 interface PropsBasket {
   stateMask: boolean;
   setMask: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-const Favorite = ({ stateMask,setMask }: PropsBasket) => {
+const Favorite = ({ stateMask, setMask }: PropsBasket) => {
   return (
     <>
       <Header stateMask={stateMask} setMask={setMask} />
-      <SectionBasket card={<CardFavorite />} />
+      <SectionBasket
+        card={<CardFavorite />}
+        swiper={<SimilarBook title={'Popular Books'} />}
+      />
       <Footer />
     </>
   );
